863a997a1c2bb789e4d91b0b5b3dbd4b7885d3e5
Ghania-Aitdriss <aitdgha77@hotmail.fr>
2022-03-01 23:00:48 +0100

    profile Typical and resume

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typical from 'react-typical'
 
 function Profile() {
   return (
@@ -16,11 +17,53 @@ function Profile() {
               <i className="fa fa-instagram"></i>
             </a>
             <a href="www.linkedin.com/in/ghania-ait-driss-b9694a231">
-            <i className="fa fa-linkedin"></i>
+              <i className="fa fa-linkedin"></i>
             </a>
             <a href="https://twitter.com/i/flow/login">
               <i className="fa fa-twitter"></i>
             </a>
+          </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">Ghania</span>
+            </span>
+            
+          </div>
+
+          <div className="profile-details-role">
+              <span className="primary-text">
+                {" "}
+                <h1>
+                    {" "}
+                <Typical 
+                loop={Infinity}
+                steps={[
+                    "Ghania Ait driss ",
+                    1000, 
+                    "Full Stack Developer",
+                    1000, 
+                    "Mern Stack Dev",
+                    1000, 
+                    "React Dev",
+                    1000,
+                ]}
+                />
+                </h1>
+                <span className="profile-role-tagline">
+                    Knack of building applications with front and backend operations.
+                </span>
+              </span>
+              <div className='profile-options'>
+                  <button className="btn primary-btn">
+                  {" "}
+                      Hire Me {" "}
+                  </button>
+                  <a href="cv.pdf" download='cv.pdf'>
+                      <button className="btn btn-primary">download Resume</button>
+                  </a>
+              </div>
           </div>
         </div>
       </div>
